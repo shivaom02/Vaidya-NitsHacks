1eee6951aebd3f2522a5fef52728562bd1921b59
Shivam Kumar <67912385+shivaom02@users.noreply.github.com>
2021-04-27 12:30:58 +0530

    Initail models added

---
+++ models/dr.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+
+const Schema = mongoose.Schema;
+
+//creating doctor schema
+const drProfileSchema = new Schema({
+    drName: String,
+    drDegree: String,
+    fieldexpertise: String,
+    workAddress: String,
+    email: String,
+    phone: String,
+    workExp: String,
+    img: {
+        data: Buffer,
+        type: String,
+    },
+    desc: String,
+}, { typeKey: '$type' });
+
+mongoose.model('Doctor', drProfileSchema);
